1d65af2abaa4c714e379275940f207622bcc2530
NaLa0x48 <86408193+NaLa0x48@users.noreply.github.com>
2022-02-28 21:51:25 +0630

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,6 @@
+import {useState} from 'react'
 const TodoList = ({todos,setTodos}) => {
+    const [todoItem,setTodoItem] = useState("sty");
     const handleDelete = ({id}) =>{
         setTodos(todos.filter((todo)=>todo.id !== id));
     }
@@ -6,18 +8,24 @@ const TodoList = ({todos,setTodos}) => {
         setTodos(todos.map((item)=>{
             if(item.id === todo.id){
                 return {...item,completed:!item.completed}
+
             }
             return item;
             
-        })
+        }
+        )
         );
-        console.log(todo);
+        if(todo.completed){
+            setTodoItem("sty1");
+        }
+        else
+            setTodoItem("sty");
         
     } 
     return (
              <div className="">
                 {todos.map((todo) =>(
-                    <li key={todo.id} className="list-item"><span className="todoItem">{todo.title}</span>
+                    <li key={todo.id} className="list-item"><span className={todoItem}>{todo.title}</span>
                 
                     <button clasName="complete-btn"onClick={()=>handleComplete(todo)}>Complete</button>
                     <button className="delete-btn" onClick={()=>handleDelete(todo)}>Delete</button></li>
