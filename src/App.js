877c26ef68676521e59e3ec16a435864a53e49db
NaLa0x48 <86408193+NaLa0x48@users.noreply.github.com>
2022-03-01 20:00:26 +0630

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import Form from './components/Form'
 import TodoList from './components/TodoList';
 import { useState } from 'react';
 function App() {
-  const [input,setInput] = useState("");
   const [todos,setTodos] = useState([]);
   return (
     <div className="App">
       <Header />
       <div className='container'>
-        <Form input={input} setInput={setInput}
-          todos={todos} setTodos={setTodos}/>
+        <Form todos={todos} setTodos={setTodos}/>
         <TodoList todos={todos} setTodos={setTodos}/>
       </div>
      
